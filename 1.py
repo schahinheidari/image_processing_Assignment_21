fcd8caa34c5f99dd2609502e79773db781f7583f
schahin0001 <schahinheidari@gmail.com>
2022-03-14 21:46:47 +0100

    update assignment

--- 1.py
+++ 1.py
@@ -1,16 +1,14 @@
 import cv2 as cv
 
 img = cv.imread("Black_photo.jpg")
-new_img = cv.resize(img, (400, 400))
+new_img = cv.resize(img, (800, 800))
 
-for i in range(400):
-    for j in range(400):
-        if i % 2 == 0:
-            new_img[i,j] = 0
-        elif j % 2 == 0:
-            new_img[i,j] = 0
+for i in range(0,800,100):
+    for j in range(0,800,100):
+        if ((i+j)/4)%2 == 0:
+            new_img[i:i+100,j:j+100] = 255
         else:
-            new_img[i,j] = 255
+            new_img[i:i+100,j:j+100] = 0
 
 
 cv.imwrite("result.jpg", new_img)
